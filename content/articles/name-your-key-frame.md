f98634eed8acad614a49264e9a8850f22b12f310
Scott Fryxelo <sfryxell@me.com>
2022-02-24 11:15:08 -0800

    Remove files from my personal blog

--- content/articles/name-your-key-frame.md
+++
@@ -1,8 +0,0 @@
----
-date: 2021-12-01
-img: /posters/Scott_Fryxell_posters_Saturday, April 17, 10_47 AM.svg
----
-
-Let's say you are building an animation and you can describe the objects inside each keyframe. Your focused on a landscape. Those hills would come to life with a pattern.
-
-It would be fun if you can name a pattern [houses-on-hills] and — though it doesn't yet exist, save it to your keyframe. Realness can turn the description into a challenge that people on realness can help realize.
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,8 @@
 <template lang="html">
-  <main>
-    <header>
-      <h1><NuxtLink to="/">Scott Fryxell</NuxtLink></h1>
-      <nav>
-        <NuxtLink to="/resume">Work</NuxtLink>
-        <a hidden href="https://cash.app/$ScottFryxell">$</a>
-      </nav>
-    </header>
-    <Nuxt/>
-    <footer/>
-  </main>
+  <Nuxt/>
 </template>
 <style lang="stylus">
   [outline],
   [outline] *
     outline: 0.15vmin dashed grey
-  main
-    margin: auto
-    flex-direction: column
-    & > header
-      display: flex
-      & > h1
-        padding: 1rem
-      & > nav
-        align-self: center
-        & > a
-          margin-right: 1rem
-    & > footer
-      padding: 1em
-
 </style>
